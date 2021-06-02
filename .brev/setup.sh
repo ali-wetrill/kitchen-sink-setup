256036c149ef5c17da9eebb492fecca24c908501
Ali <ali.ahmed@berkeley.edu>
2021-06-02 18:26:59 -0400

    Update setup.sh

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -7,8 +7,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "zsh" >> ~/.bashrc
 
 # install node
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+# sudo apt-get install -y nodejs
 
 # to use makefiles
 sudo apt-get install -y build-essential
